7d54a369d1e0bfa0e7f160e079789d010df7def4
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-15 23:46:39 +0200

    Get axios

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
+  let [value, setValue] = useState(null);
+  let apiUrl = `https://potterapi-fedeperin.vercel.app/en/${value}`;
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
+  function handleEvent(event) {
+    event.preventDefault();
+    axios.get(apiUrl).then(handleResponse);
+  }
   return (
     <div className="Search">
-      <form>
+      <form onSubmit={handleEvent}>
         <input
           type="search"
           placeholder="Ex. Weasley, spells"
           className="search"
+          onChange={handleChange}
         />
         <input type="submit" value="Search" className="submit" />
       </form>
