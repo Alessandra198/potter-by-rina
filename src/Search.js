0c047fadef130ac32287f776e64056f49f1a386c
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-15 23:53:23 +0200

    Updated

--- src/Search.js
+++ src/Search.js
@@ -4,10 +4,10 @@ import "./Search.css";
 
 export default function Search() {
   let [value, setValue] = useState(null);
-  let apiUrl = `https://potterapi-fedeperin.vercel.app/en/${value}`;
+  let apiUrl = `https://potterapi-fedeperin.vercel.app/en/${value}/max`;
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data);
   }
 
   function handleChange(event) {
@@ -16,6 +16,8 @@ export default function Search() {
 
   function handleEvent(event) {
     event.preventDefault();
+    console.log(value);
+
     axios.get(apiUrl).then(handleResponse);
   }
   return (
